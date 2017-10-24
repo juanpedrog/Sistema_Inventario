469cf3b9302e06aa1a0567c8f8013b865273b6fb
Juan Pedro Gil Llanos <juanpedrogilllanos@hotmail.com>
2017-10-24 17:31:36 -0600

    Primer commit
    Estructura del proyecto con la primera interfaza del log in.

---
+++ ProyectoGPS/src/Login.java
@@ -0,0 +1,24 @@
+
+import com.sun.awt.AWTUtilities;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Login {
+    
+    /**
+     * *@param args the command line arguments
+     */
+    public static void main(String[] args){
+        VenLogin vl=new VenLogin();
+        AWTUtilities.setWindowOpaque(vl,false);
+        vl.setVisible(true);
+    }
+}
