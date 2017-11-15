8428b76996c8550d4b071502137083e30ef59447
Juan Pedro Gil Llanos <juanpedrogilllanos@hotmail.com>
2017-11-15 10:16:08 -0700

    CrEACION DE PDF COMPLETA

--- ProyectoGPS/src/CrearPDF.java
+++ ProyectoGPS/src/CrearPDF.java
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -267,11 +268,54 @@ public class CrearPDF {
         cb.setFontAndSize(bfNoNegritas,12);
         cb.setTextMatrix(50,330+espacio);
         cb.showText("Entre los dias: ");
-        //Label fecha salida
+        //Fecha salida base de datos
         cb.setFontAndSize(bf,12);
-        cb.setTextMatrix(90,330+espacio);
+        cb.setTextMatrix(160,330+espacio);
         datos=conexion.acceder("select S.fecha_salida from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
         cb.showText(datos.get(0));
+        //Label al
+        cb.setFontAndSize(bfNoNegritas, 12);
+        cb.setTextMatrix(240,330+espacio);
+        cb.showText(" al ");
+        //Fecha llegada base de datos
+        cb.setFontAndSize(bf, 12);
+        cb.setTextMatrix(270,330+espacio);
+        datos=conexion.acceder("select S.fecha_llegada from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+        cb.showText(datos.get(0));
+        //Label pernoctado
+        cb.setFontAndSize(bfNoNegritas, 12);
+        cb.setTextMatrix(360,330+espacio);
+        cb.showText("Pernoctado: ");
+        //Pernoctado base de datos
+        cb.setFontAndSize(bf, 12);
+        cb.setTextMatrix(445,330+espacio);
+        datos=conexion.acceder("select S.pernoctado from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+        cb.showText(datos.get(0));
+        //Label firma de recibido
+        cb.setFontAndSize(bfNoNegritas, 12);
+        cb.setTextMatrix(100,230+espacio);
+        cb.showText("Firma de recibido");
+        //Label firma de autorizado
+        cb.setTextMatrix(350,230+espacio);
+        cb.showText("Firma de autorizado");
+        //Label linea de firma
+        cb.setTextMatrix(70,160+espacio);
+        cb.showText("__________________________");
+        cb.setTextMatrix(320,160+espacio);
+        cb.showText("__________________________");
+        //Label comisionado
+        cb.setTextMatrix(120,140+espacio);
+        cb.showText("Comisionado");
+        //Presidente base de datos
+        cb.setFontAndSize(bf, 12);
+        cb.setTextMatrix(320,140+espacio);
+        datos=conexion.acceder("select * from director_general");
+        cb.showText(datos.get(0));
+        //Label presidente
+        cb.setFontAndSize(bfNoNegritas,12);
+        cb.setTextMatrix(350,120+espacio);
+        cb.showText("Consejero presidente");
+        
         
         //Fin del contenido
         cb.endText();
@@ -280,6 +324,156 @@ public class CrearPDF {
         }catch(IOException e){
             
         }
+    }
+    public void oficio_comision(String folio) throws DocumentException{
+        String path="C:\\Users\\Juan Pedro Gil\\Desktop\\prueba.pdf";
+        try{
+            File f=new File(path);
+            f.delete();
+            Document document=new Document(PageSize.A4,0f,0f,0f,0f);
+            PdfWriter writer=PdfWriter.getInstance(document, new FileOutputStream(path));
+            ArrayList<String> datos;
+            Conexion conexion=new Conexion();
+            conexion.conexion();
+            document.open();
+            BaseFont bf=BaseFont.createFont(BaseFont.COURIER_BOLD,BaseFont.CP1252,BaseFont.NOT_EMBEDDED);
+            BaseFont bfNoNegritas=BaseFont.createFont(BaseFont.COURIER,BaseFont.CP1252,BaseFont.NOT_EMBEDDED);
+            PdfContentByte cb=writer.getDirectContent();
+            cb.setFontAndSize(bf,16);
+            Image image=Image.getInstance("C:\\Users\\Juan Pedro Gil\\Documents\\GitHub\\Sistema_Viaticos\\ProyectoGPS\\src\\Imagenes\\icono.png");
+            document.add(image);
+            cb.beginText();
+            //Label Folio
+            cb.setFontAndSize(bfNoNegritas, 12);
+            cb.setTextMatrix(480,800);
+            cb.showText("Folio: ");
+            //Folio base de datos
+            cb.setFontAndSize(bf, 12);
+            cb.setTextMatrix(525,800);
+            datos=conexion.acceder("select folio from oficio_comision where folio="+folio);
+            cb.showText(datos.get(0));
+            //Label oficio de comisión
+            cb.setFontAndSize(bfNoNegritas, 16);
+            cb.setTextMatrix(200,750);
+            cb.showText("Oficio de comisión");
+            //Label nombre
+            cb.setFontAndSize(bfNoNegritas, 12);
+            cb.setTextMatrix(50,700);
+            cb.showText("Nombre:");
+            //Nombre base de datos
+            cb.setFontAndSize(bf, 12);
+            cb.setTextMatrix(120,700);
+            datos=conexion.acceder("select S.nombre from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            cb.showText(datos.get(0));
+            //Label puesto
+            cb.setFontAndSize(bfNoNegritas, 12);
+            cb.setTextMatrix(50,675);
+            cb.showText("Puesto:");
+            //Puesto base de datos
+            int size=12;
+            cb.setFontAndSize(bf, 12);
+            cb.setTextMatrix(120,675);
+            datos=conexion.acceder("select S.puesto from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            cb.showText(datos.get(0));
+            //Label presente
+            cb.setFontAndSize(bfNoNegritas, 16);
+            cb.setTextMatrix(50,625);
+            cb.showText("PRESENTE:");
+            //Label lugar y fecha
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(50,600);
+            cb.showText("Con este lugar y fecha: ");
+            //Label lugar
+            cb.setFontAndSize(bf,size);
+            cb.setTextMatrix(220,600);
+            Calendar cal= Calendar.getInstance();
+            String fecha="";
+            fecha=cal.get(cal.DATE)+"/"+(cal.get(cal.MONTH)+1)+"/"+cal.get(cal.YEAR);
+            cb.showText("Tepic, Nayarit, "+fecha);
+            //Label descripción
+            String cad="Ha sido comisionado por el suscrito, a efecto de llevar a cabo visita de trabajo: ";
+            acomodar_Actividad(cad,cb,bfNoNegritas,550);
+            //Label desde los dias
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(50,500);
+            cb.showText("Desde los días ");
+            //Fecha salida base de datos
+            cb.setFontAndSize(bf, size);
+            cb.setTextMatrix(160,500);
+            datos=conexion.acceder("select S.fecha_salida from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            cb.showText(datos.get(0));
+            //Label al
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(240,500);
+            cb.showText("al");
+            //Fecha llegada base de datos
+            cb.setFontAndSize(bf, size);
+            cb.setTextMatrix(265,500);
+            datos=conexion.acceder("select S.fecha_llegada from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            cb.showText(datos.get(0));
+            //Label en la localidad de
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(50,475);
+            cb.showText("En la localida de:");
+            //Lugar base de datos
+            cb.setFontAndSize(bf, size);
+            cb.setTextMatrix(185,475);
+            datos=conexion.acceder("select S.lugar from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            cb.showText(datos.get(0));
+            //Label que consiste en:
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(50,450);
+            cb.showText("Que consiste en:");
+            //Actividad base de datos
+            cb.setFontAndSize(bf, size);
+            cb.setTextMatrix(180,450);
+            datos=conexion.acceder("select S.actividad from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            acomodar_Actividad(datos.get(0),cb,bf,425);
+            datos=conexion.acceder("select S.vehiculo from solicitud S INNER JOIN oficio_comision O ON S.idSolicitud=O.Solicitud_idSolicitud WHERE folio="+folio);
+            //Vehiculo
+            int espacio=0;
+            if(!datos.get(0).equals("Seleccione el vehículo")){
+                espacio=-60;
+                cb.setFontAndSize(bfNoNegritas, size);
+                cb.setTextMatrix(50,350);
+                cb.showText("Vehiculo:");
+                //Vehiculo base de datos
+                cb.setFontAndSize(bf, size);
+                cb.setTextMatrix(150,350);
+                cb.showText(datos.get(0));
+            }
+            //Label autorizado
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(230,350+espacio);
+            cb.showText("Firma autorizado");
+            //Label linea
+            cb.setTextMatrix(200,280+espacio);
+            cb.showText("_________________________");
+            //Director general
+            cb.setFontAndSize(bf, size);
+            cb.setTextMatrix(190,260+espacio);
+            datos=conexion.acceder("select * from director_general");
+            cb.showText(datos.get(0));
+            //Label consejero presidente
+            cb.setFontAndSize(bfNoNegritas, size);
+            cb.setTextMatrix(210,240+espacio);
+            cb.showText("Consejero presidente");
+            
+            
+            
+            //Fin del contenido
+            cb.endText();
+            document.close();
+            Desktop.getDesktop().open(f);
+        }catch(IOException e){
+            
+        }
+    }
+    public static void main(String[] args){
+        CrearPDF a=new CrearPDF();
+        try{
+            a.oficio_comision("1");
+        }catch(Exception e){}
     }
 }
 
